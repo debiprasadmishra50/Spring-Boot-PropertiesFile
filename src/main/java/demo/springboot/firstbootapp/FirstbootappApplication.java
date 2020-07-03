bff3872185a4cc8290424bd9550a946302be280e
debiprasadmishra50 <debiprasadmishra50@gmail.com>
2020-07-03 20:39:25 +0530

    Spring-Boot-PropertiesFile

---
+++ src/main/java/demo/springboot/firstbootapp/FirstbootappApplication.java
@@ -0,0 +1,13 @@
+package demo.springboot.firstbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class FirstbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstbootappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/springboot/firstbootapp/FirstbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springboot.firstbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
